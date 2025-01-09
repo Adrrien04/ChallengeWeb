ea40b91538ce8d12e881ea458a76f99b20d15853
fluffykoo <oumou.camara@efrei.net>
2025-01-09 11:33:20 +0100

    Updates

--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -1,18 +1,42 @@
 <?php
+// filepath: /Users/oumoukeirouc./Documents/my-api-dailyquest/src/Controller/ApiUserController.php
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ApiUserController extends AbstractController
 {
-    #[Route('/api/user', name: 'app_api_user')]
-    public function index(): Response
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
     {
-        return $this->render('api_user/index.html.twig', [
-            'controller_name' => 'ApiUserController',
-        ]);
+        $this->client = $client;
+    }
+
+    #[Route('/users', name: 'app_user_list')]
+    public function userList(): Response
+    {
+        try {
+            $response = $this->client->request('GET', 'https://localhost:8002/api/users', [
+                'timeout' => 10,  // Délai plus court pour éviter un timeout trop long
+                'verify_peer' => false,  // Désactiver la vérification du certificat SSL
+                'verify_host' => false,  // Désactiver la vérification de l'hôte SSL
+            ]);
+
+            // Convertir la réponse JSON en tableau
+            $data = $response->toArray();
+
+            // Passer les données des utilisateurs à la vue
+            return $this->render('user_list.html.twig', [
+                'users' => $data['member'],
+            ]);
+        } catch (\Exception $e) {
+            // Gérer les exceptions et retourner une réponse d'erreur
+            return new Response('Erreur lors de la récupération des utilisateurs : ' . $e->getMessage(), 500);
+        }
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ApiResource]  // Cette annotation permet d'exposer l'entité via l'API
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -62,5 +63,22 @@ class User
         $this->mdp = $mdp;
 
         return $this;
+    }
+
+    // Implémentation de la méthode getRoles()
+    public function getRoles(): array
+    {
+        // Les utilisateurs auront toujours un rôle par défaut "ROLE_USER"
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword(): string
+    {
+        return $this->mdp;
+    }
+
+    public function eraseCredentials()
+    {
+        // Si l'utilisateur a des données sensibles à effacer, les ajouter ici
     }
 }
